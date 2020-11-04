bed410583e6398b689ea9a127267202d67dee5a0
Daniel Chilcott <daniel@flowgear.net>
2020-11-04 11:30:24 +0200

    fix namespace

--- custom-apps/HelloWorldApp/Program.cs
+++ custom-apps/HelloWorldApp/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Rymco_PORequisitions
+namespace HelloWorldApp
 {
     public class Program
     {
--- custom-apps/HelloWorldApp/Startup.cs
+++ custom-apps/HelloWorldApp/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Rymco_PORequisitions
+namespace HelloWorldApp
 {
     public class Startup
     {
