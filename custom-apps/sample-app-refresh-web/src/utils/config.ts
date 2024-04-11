dd9325ace540381d7a9fe48f420539eb61a7ebed
Rikard Schouwstra <rjschouwstra11@gmail.com>
2024-04-11 13:27:58 +0200

    Removed demo URL from config

--- custom-apps/sample-app-refresh-web/src/utils/config.ts
+++ custom-apps/sample-app-refresh-web/src/utils/config.ts
@@ -1,4 +1,4 @@
 export default class Config {
-    static baseUrl: string = "https://demodomain2023sa.flowgear.net/";
-    static baseUrlDev: string = "https://demodomain2023sa-test-depjudiy.flowgear.net/";
+    static baseUrl: string = "";
+    static baseUrlDev: string = "";
 }
